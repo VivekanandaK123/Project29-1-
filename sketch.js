7e270121cdd670cc4ee88cde241a4181236d182a
VivekanandaK123 <vivekanandakatakam16@gmail.com>
2020-07-03 15:42:51 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var ground, block;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+ 
+
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(300,300,100,10);
+  stand = new Ground(500,200,100,10);
+
+  block1 = new Block(320,275,40,50);
 }
 
 function draw() {
   background(255,255,255);  
-  drawSprites();
+  Engine.update(engine);
+ 
+  ground.display();
+  stand.display();
+  block1.display();
 }
